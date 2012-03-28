85cda9c15e4e455c75ed0d752fb3d49b65aa50c7
luffycn <kingla_pei@163.com>
2012-03-28 16:27:16 +0800

    Commit

--- gen/com/android/story/R.java
+++ gen/com/android/story/R.java
@@ -14,7 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int buttonDelete=0x7f050003;
+        public static final int btnDelete=0x7f050003;
         public static final int contentlist=0x7f050001;
         public static final int newProduct=0x7f050000;
         public static final int text=0x7f050002;
